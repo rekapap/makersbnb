3cb3dbddbff216b195e36064f57b42900a9c0915
Todd Langford-Archer <toddpla@gmail.com>
2018-11-05 14:50:07 +0000

    added unit test for Space::create

--- spec/features/adding_space_spec.rb
+++ spec/features/adding_space_spec.rb
@@ -3,7 +3,7 @@ feature "adding space" do
     visit '/spaces/new'
     fill_in("description", with: "two bed flat in Tooting")
     fill_in("price", with: "100")
-    click_button("submit")
+    click_button("Submit")
     expect(page).to have_content("two bed flat in Tooting")
   end
 end
