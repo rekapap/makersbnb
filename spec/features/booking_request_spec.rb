79f74ad45d1e74ef3b7811d29b1134dd21572262
Todd Langford-Archer <toddpla@gmail.com>
2018-11-06 14:53:00 +0000

    user can request booking

--- spec/features/booking_request_spec.rb
+++ spec/features/booking_request_spec.rb
@@ -1,9 +1,19 @@
 feature "tenant makes booking request" do
- scenario "booking request gets made" do
+ scenario "booking request successfully made" do
    signup
    create_space
    select_space
    create_booking
-   expect(page).to have_content("Your booking has been sent")
+   expect(page).to have_content("Booking requested")
+ end
+ scenario "booking request rejected as space unavailale" do
+   signup
+   create_space
+   select_space
+   create_booking
+   visit('/spaces')
+   select_space
+   create_booking
+   expect(page).to have_content("Booking not available for this date")
  end
 end
