570f29b57d504ff6d17c41ad7cd186fa4ee560b2
Reka <rekapap28@gmail.com>
2018-11-07 00:25:30 +0000

    typofix

--- lib/mailer.rb
+++ lib/mailer.rb
@@ -1,16 +1,14 @@
 require 'pony'
 
 class Mailer
-
   def self.send(email:, subject:, message:)
     Pony.mail(
       :to => email,
-      :from => '"Chitter" <email@chitter.com>',
+      :from => '"MakersBNB" <email@makersbnb.com>',
       :subject => subject,
       :via => :sendmail,
       :via_options => { :location => '/usr/sbin/sendmail', :arguments => '-t' },
       :html_body => message
     )
   end
-
 end
